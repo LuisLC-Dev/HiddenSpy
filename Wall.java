b7308918180b510dd02ef8b4f6948368eb8c0e69
LuisLC-Dev <lopezluis2c@gmail.com>
2021-06-14 20:19:29 -0500

    correccion de las paredes

--- Wall.java
+++ Wall.java
@@ -12,6 +12,10 @@ public class Wall extends Actor
      * Act - do whatever the Wall wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Wall(){
+    setImage("images/general-wall.png");
+    }
+    
     public void act() 
     {
         // Add your action code here.
